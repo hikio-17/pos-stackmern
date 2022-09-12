c679f19b2682966416d9d403c9ba3d9ce678238e
hikio-17 <fajritio376@gmail.com>
2022-09-11 21:56:53 -0700

    change

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
-app.get("/", (req, res) => {
-  res.send("<h1>POS BACKEND</h1>");
-});
+
 app.use("/api/items", itemRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/bills", billsRoutes);
