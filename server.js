c2f6361579513aba02398a48b30383f2ab6745e9
hikio-17 <fajritio376@gmail.com>
2022-09-12 22:46:59 -0700

    change file server.js

--- server.js
+++ server.js
@@ -32,14 +32,21 @@ app.use("/api/items", itemRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/bills", billsRoutes);
 
-// deployment
-// __dirname = path.resolve();
+// --------------------------deployment------------------------------
+__dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
   });
 }
+// --------------------------deployment------------------------------
 
 // port
 const PORT = process.env.PORT || 5000;
